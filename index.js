129393702906cf048ad6261c5a4b2b533e0c5c2c
nia realia <wearehors@gmail.com>
2017-08-07 17:50:06 -0700

    glossary functionality

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+glossary = ["3d printing",
+			"additivism",
+			"computer vision",
+			"cryptocurrency",
+			"cyberfeminism", 
+			"cybersecurity",
+			"cybersurveillance",
+			"encryption",
+			"ether",
+			"facial recognition",
+			"gynepunk",
+			"hacktivism",
+			"open source",
+			"phishing",
+			"pirate radio",
+			"poetic terrorism",
+			"private key",
+			"monkeywrenching",
+			"reproductive justice", 
+			"remote access trojan",
+			"social engineering",
+			"sousveillance",
+			"speculative design",
+			"tunneling protocol"
+			];
+
+colors = ["rgba(87,186,225,1)", 
+			"rgba(192,210,64,1)", 
+			"rgba(228,65,70,1)", 
+			"rgba(145,41,141,1)", 
+			"rgba(87,186,225,1)"];
+
 pastIntro = false;
 
 function MirrorType() {
@@ -25,6 +57,7 @@ function EnterCommand() {
 
 		$("#mainContent").fadeIn("slow");
 		$("#logo").addClass("accessed");
+		fillInGlossary();
 
 		pastIntro = true;
 	}
@@ -38,6 +71,16 @@ function EnterCommand() {
 	commandWindow.appendChild(fakeCursor);
 	typed = newtyped;
 	youTyping.value = "";
+}
+
+function fillInGlossary() {
+	glossaryDom = document.getElementById("glossary");
+	
+	glossary.forEach(function(term) {
+		termColor = colors[Math.floor(Math.random()*colors.length)];
+    	var markup = "<a class='glossaryTerm' rel='nofollow' style='color: " + termColor + "' href='https://duckduckgo.com/?q=" + term + "'>" + term + "</a>";
+    	glossaryDom.innerHTML = glossaryDom.innerHTML + markup;
+	});
 }
 
 $(document).ready(function() {
